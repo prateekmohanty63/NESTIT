5d190e1760afdbaa9915e13161074fe6bd9b8d6b
Prateek <prateekmohanty63@gmail.com>
2022-08-05 22:52:15 +0530

    Completed student controllers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import {StudentController} from '../student/student.controller'
 import { TeacherController} from '../teacher/teacher.controller'
 import {StudentTeacherController} from '../teacher/student.controller'
+import { StudentService } from 'src/student/student.service';
 
 
 @Module({
   imports: [],
-  controllers:[StudentController,TeacherController,StudentTeacherController]
+  controllers:[StudentController,TeacherController,StudentTeacherController],
+  providers:[StudentService]
  
 })
 export class AppModule {}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,11 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { students } from 'src/db';
+import { CreateStudentDto, FindStudentsResponseDto, StudentResponseDto, UpdateStudentDto } from './dto/student.dto';
+import {v4 as uuid} from "uuid"
 
 @Injectable()
 export class StudentService {
-    students=students
-    getStudents()
+    private students=students
+    getStudents():FindStudentsResponseDto[]
     {
         return this.students
+    }
+    getStudentById(studentId:string):FindStudentsResponseDto{
+          return this.students.find(student=>{
+              return student.id===studentId
+          })
+    }
+
+    createStudent(payload:CreateStudentDto):StudentResponseDto{
+          let newStudent={
+              id:uuid(),
+              ...payload
+          }
+          this.students.push(newStudent)
+
+          return newStudent
+    }
+    updateStudent(payload:UpdateStudentDto,studentId:string){
+        
+        let updateStudent:StudentResponseDto;
+
+        const updatedStudentList=this.students.map(student=>{
+            if(student.id==studentId){
+                updateStudent={
+                    id:studentId,
+                    ...payload
+                } 
+            }
+            else return student
+        })
+
+        this.students=updatedStudentList;
+
+        return updateStudent
     }
 }
