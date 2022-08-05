130afc4bccc9b721736eeaa4cb645216ab80df26
Prateek <prateekmohanty63@gmail.com>
2022-08-05 22:31:16 +0530

    Added getStudent provider

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { students } from 'src/db';
 
 @Injectable()
 export class StudentService {
-    
+    students=students
+    getStudents()
+    {
+        return this.students
+    }
 }
