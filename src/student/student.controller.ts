a725e7a993afa1781f831b3dd644d31f6f57a374
Prateek <prateekmohanty63@gmail.com>
2022-08-05 14:21:28 +0530

    Added DTO's for teacher and student

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,26 +1,26 @@
 import {Controller,Get, Post, Put,Param,Body} from "@nestjs/common";
-
+import {CreateStudentDto,UpdateStudentDto, FindStudentsResponseDto} from './dto/student.dto'
 
 
 @Controller('students')
 export class StudentController{
     @Get()
-   getStudents(){
+   getStudents(): FindStudentsResponseDto[]{
        return "All Students"
    }
    
    @Get('/:studentId')
    getStudentById(
        @Param('studentId') studentId:string
-   ){
+   ): FindStudentsResponseDto{
        console.log(studentId)
       return `Get Student by Id ${studentId}`
    }
    
    @Post()
    createStudent(
-       @Body() body
-   ){
+       @Body() body:CreateStudentDto
+   ): FindStudentsResponseDto{
        console.log(body)
        return `Create Student With the Following Data ${JSON.stringify(body)}`
    }
@@ -28,7 +28,7 @@ export class StudentController{
   @Put('/:studentId')
    updateStudent(
        @Param('studentId') studentId:string,
-       @Body() body
+       @Body() body:UpdateStudentDto
    ){
        return `Update student with id ${studentId} with data of ${JSON.stringify(body)}`
    }
